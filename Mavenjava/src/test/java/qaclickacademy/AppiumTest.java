db4b8077393d5c17e63affee9e551f405193d1c2
rachana <rachanasharma0910@gmail.com>
2021-08-19 13:12:06 +0530

    first commit

---
+++ Mavenjava/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	 @Test
+	    public void PostJita()
+	    {
+			System.out.println("PostJita");
+
+	    }
+	    @Test
+	    public void deleteTwitter()
+	    {
+			System.out.println("deleteTwitter");
+
+	    }
+
+}
